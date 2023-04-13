44b780b280729c07e3705403adea5b35c00267b3
Rahul Kumar Barnwal <rahulbarnwalonlyu2@gmail.com>
2023-04-13 12:54:25 +0530

    Update transactionController.js
    error handling

--- controllers/transactionController.js
+++ controllers/transactionController.js
@@ -20,10 +20,10 @@ const getAllTransaction = async (req, res) => {
       ...(type !== "all" && { type }),
       userid,
     });
-    res.status(200).send(transactions);
+    return res.status(200).send(transactions);
   } catch (err) {
     console.log(err);
-    res.status(500).json(err);
+    return res.status(500).send(err);
   }
 };
 
@@ -34,10 +34,10 @@ const editTransaction = async (req, res) => {
       { _id: transactionId },
       req.body.payload
     );
-    res.status(200).send('Edit Successfully')
+    return res.status(200).send('Edit Successfully')
   } catch (err) {
     console.log(err);
-    res.status(500).json(err);
+    return res.status(500).send(err);
   }
 };
 
@@ -45,10 +45,10 @@ const addTransaction = async (req, res) => {
   try {
     const newTransaction = new transactionModel(req.body);
     await newTransaction.save();
-    res.status(201).send("Transaction Created");
+    return res.status(201).send("Transaction Created");
   } catch (err) {
     console.log(err);
-    res.status(500).json(err);
+    return res.status(500).send(err);
   }
 };
 
@@ -56,10 +56,10 @@ const deleteTransaction =async (req, res) => {
     try {
         const { transactionId } = req.body;
         await transactionModel.findOneAndDelete({ _id: transactionId })
-        res.status(200).send('Transaction deleted successfully!');
+        return res.status(200).send('Transaction deleted successfully!');
     } catch (err) {
         console.log(err);
-        res.status(500).json(err);
+        return res.status(500).send(err);
     }
 };
 
